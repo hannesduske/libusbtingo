6d3848403be5a810e4d4335a436b3a5b66a31ad5
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2025-01-15 13:43:49 +0100

    test(can): add unit test for Dlc class

---
+++ test/can/unittest-Message.cpp
@@ -0,0 +1,35 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <memory>
+#include <cstdint>
+#include "usbtingo/can/Message.hpp"
+
+// Convenience
+using usbtingo::can::Message;
+
+// Testcase #1
+TEST_CASE("Unittest Message", "[message]"){
+
+    SECTION("Instantiate empty Message"){
+        auto msg = Message();
+
+        CHECK(msg.id == 0);
+        CHECK(msg.data.empty());
+    }
+    
+    SECTION("Instantiate generic Message #1"){
+        auto msg = Message(42, {0x00, 0x01, 0x02, 0xff});
+
+        CHECK(msg.id == 42);
+        CHECK(msg.data.size() == 4);
+    }
+
+    SECTION("Instantiate generic Message #1"){
+        std::uint32_t testid = 42;
+        auto testdata = std::vector<std::uint8_t>({0x00, 0x01, 0x02, 0xff});
+        auto msg = Message(testid, testdata);
+
+        CHECK(msg.id == testid);
+        CHECK(msg.data == testdata);
+    }
+}
