92f3bee4ee0d97625e4ad9e326f2209899232af8
Hannes Duske <152866008+hannesduske@users.noreply.github.com>
2025-12-13 12:20:05 +0100

    test: update tests for new DeviceFactory behaviour

--- tests/device/unittest-DeviceFactory.cpp
+++ tests/device/unittest-DeviceFactory.cpp
@@ -8,8 +8,17 @@ using usbtingo::device::DeviceFactory;
 
 TEST_CASE("Unittest DeviceFactory", "[device_factory]") {
 
+  SECTION("Instantiate valid device from serial number 0") {
+    constexpr std::uint32_t TEST_SN = 0;
+
+    auto dev = DeviceFactory::create(TEST_SN);
+    CHECK(dev != nullptr);
+    CHECK(dev->is_alive());
+    
+  }
+
   SECTION("Instantiate invalid device through factory") {
-    std::vector<std::uint32_t> sn_vec = { 0, 1, 42 };
+    std::vector<std::uint32_t> sn_vec = {1, 42 }; // Not 0!
 
     for (const auto sn : sn_vec) {
       auto dev = DeviceFactory::create(sn);
